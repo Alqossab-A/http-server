9ee61b3787e03940a585e742ccfdd1b021fa8dc4
Ahmed <alqossaba@gmail.com>
2024-05-27 00:19:41 -0400

    inital commit

---
+++ app/main.ts
@@ -0,0 +1,39 @@
+import * as net from 'net';
+
+const server = net.createServer((socket: any) => {
+	console.log('Client is connected');
+
+	socket.on('data', (data: any) => {
+		const dataStr = data.toString();
+		console.log('Data received: ', dataStr);
+
+		const path = dataStr.split('\r\n')[0].split(' ')[1];
+		console.log('path', path);
+
+		const query = path.split('/')[2];
+		console.log('pathBody', query);
+
+		if (path === '/') {
+			socket.write('HTTP/1.1 200 OK\r\n\r\n');
+		} else if (path === `/echo/${query}`) {
+			socket.write(`HTTP/1.1 200 OK\r\n
+                Content-Type: text/plain\r\n
+                Content-Length: ${query.length}\r\n\r\n
+                ${query}`)
+		} else {
+			socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+		}
+
+		console.log('Client diconnecting');
+		socket.end();
+	});
+});
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+
+// Uncomment this to pass the first stage
+server.listen(4221, 'localhost', () => {
+	console.log('Server is running on port 4221');
+});
+
---
+++ server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+exec bun run app/main.ts "$@"
